fe374dd676f24173372a98ad1ebd9cd5b3542ba3
dinhmartin1 <Dinh.Martin1@gmail.com>
2017-07-12 21:13:06 -0700

    7/12/17

--- src/project/martin/School.java
+++ src/project/martin/School.java
@@ -1,63 +1,68 @@
 package project.martin;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class School {
-	private String name;
-	private ArrayList<Student> freshmen;
-	private ArrayList<Student> sophomore;
-	private ArrayList<Student> senior;
-	private ArrayList<Student> reject;
+	//private String name;
+	//private ArrayList<Student> freshmen;
+	//private ArrayList<Student> sophomore;
+	//private ArrayList<Student> senior;
+	//private ArrayList<Student> reject;
 	
 	public School(String schoolName){
-		name = schoolName;
-		freshmen = new ArrayList<Student>();
-		sophomore = new ArrayList<Student>();
-		senior = new ArrayList<Student>();
-		reject = new ArrayList<Student>();
+		HashMap<String, List<Student>> registrations = new HashMap<String,List<Student>>();
+		
+		registrations.put("freshmen",new ArrayList<Student>());
+		registrations.put("sophomore",new ArrayList<Student>());
+		registrations.put("senior",new ArrayList<Student>());
+		registrations.put("reject",new ArrayList<Student>());
+
+		//name = schoolName;
+		//freshmen = new ArrayList<Student>();
+		//sophomore = new ArrayList<Student>();
+		//senior = new ArrayList<Student>();
+		//reject = new ArrayList<Student>();
 	}
 	
 	public String register(Student someone){
 		someone.getAge();
 		if(someone.getAge() <= 9){
-			freshmen.add(someone);
-			return("freshmen");
+			return freshmen;
 		}
 		else if(someone.getAge() > 9 && someone.getAge() <= 13){
-			sophomore.add(someone);
-			return("sophomore");
+			return sophomore;
 		}
 		else if(someone.getAge() > 13 && someone.getAge() <= 18){
-			senior.add(someone);
-			return("senior");
+			return senior;
 		}
 		else{
-			reject.add(someone);
-			return("reject");
+			return reject;
 		}
 	}
 	
-	public void printFreshmen(){
-		for(Student newStudent: freshmen){
-			System.out.println(newStudent.getAge());
-		}
-	}
+	//public void printFreshmen(){
+	//	for(Student newStudent: freshmen){
+	//		System.out.println(newStudent.getAge());
+	//	}
+	//}
 	
-	public void printSophomore(){
-		for(Student newStudent: sophomore){
-			System.out.println(newStudent.getAge());
-		}
-	}
+	//public void printSophomore(){
+	//	for(Student newStudent: sophomore){
+	//		System.out.println(newStudent.getAge());
+	//	}
+	//}
 	
-	public void printSenior(){
-		for(Student newStudent: senior){
-			System.out.println(newStudent.getAge());
-		}
-	}
+	//public void printSenior(){
+	//	for(Student newStudent: senior){
+	//		System.out.println(newStudent.getAge());
+		//}
+	//}
 	
-	public void printReject(){
-		for(Student newStudent: reject){
-			System.out.println(newStudent.getAge());
-		}
-	}
+	//public void printReject(){
+	//	for(Student newStudent: reject){
+	//		System.out.println(newStudent.getAge());
+	//	}
+	//}
 }
