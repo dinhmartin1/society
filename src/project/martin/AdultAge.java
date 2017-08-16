2347136e2288afd70e739a11f7c66a6fc2f3bbac
dinhmartin1 <Dinh.Martin1@gmail.com>
2017-08-16 00:07:19 -0700

    8/16

---
+++ src/project/martin/AdultAge.java
@@ -0,0 +1,18 @@
+package project.martin;
+
+public class AdultAge {
+	private int value;
+	
+	public AdultAge(int myValue) {
+		value = myValue;
+	}
+	
+	public void numberInput(){
+		if(value < 25 && value > 75) {
+			System.out.println("Numbers must be between 25 and 75.S");
+		}
+		else {
+			
+		}
+	}
+}
