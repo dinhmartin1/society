95cdf04e259f37fb30438b50173e4b3858dea239
dinhmartin1 <Dinh.Martin1@gmail.com>
2017-08-20 19:59:44 -0700

    The conditions

--- src/project/martin/CommonNumber.java
+++ src/project/martin/CommonNumber.java
@@ -1,21 +1,17 @@
 package project.martin;
 
 public class CommonNumber {
+	 
+    public static void main(String args[]) {
+    	int number1 = 52;  
+    	int number2 = 24;  
+    	String commonNumber = "";  
 
-	public static void main(String[] args) {
-		String word1 = "Huy";  
-		String word2 = "My";  
-		String sameCharacter = " ";  
-
-		for(int i = 0; i < word1.length(); i++) {  
-		    for(int j = 0; j < word2.length(); j++) {  
-		        if(word1.charAt(i) == word2.charAt(j)) {  
-		            sameCharacter += word1.charAt(i)+ " ";  
-		        }  
-		    }  
-		}  
-		System.out.println(sameCharacter);  
-
-	}
-
+    	if((number1 < 25 || number2 < 25) || (number1 > 75 || number2 > 75)) {
+    		System.out.println("Numbers must be between 25 and 75.");
+    	}
+    	else {
+    		System.out.println("The common number is: "+ commonNumber);  
+    	}
+    }
 }
