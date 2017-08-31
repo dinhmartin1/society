5855336de98542c7eee1c1a9fbdf0a55270b58dd
dinhmartin1 <Dinh.Martin1@gmail.com>
2017-08-30 20:40:44 -0700

    I think I have it

--- src/project/martin/CommonNumber.java
+++ src/project/martin/CommonNumber.java
@@ -3,14 +3,29 @@ package project.martin;
 public class CommonNumber {
 	 
     public static void main(String args[]) {
-    	int number1 = 52;  
-    	int number2 = 24;  
-    	String commonNumber = "";  
+    	int number1 = 53;  
+    	int number2 = 25; 
 
-    	if((number1 < 25 || number2 < 25) || (number1 > 75 || number2 > 75)) {
+    	if(number1 < 25 || number2 < 25 || number1 > 75 || number2 > 75) {
     		System.out.println("Numbers must be between 25 and 75.");
     	}
     	else {
+    		String numberString = "53";
+    		String numberString2 = "25";
+    		int result = Integer.parseInt(numberString);
+    		int result2 = Integer.parseInt(numberString2);
+    		System.out.println(result);
+    		System.out.println(result2);	
+    		
+    		String commonNumber = "";  
+
+    		for(int i = 0; i < numberString.length(); i++) {  
+    		    for(int j = 0; j < numberString2.length(); j++) {  
+    		        if(numberString.charAt(i) == numberString2.charAt(j)){  
+    		            commonNumber += numberString.charAt(i) + " ";  
+    		        }  
+    		    }  
+    		}  
     		System.out.println("The common number is: "+ commonNumber);  
     	}
     }
