9dc008668499cae070526af8c27052926cf4d293
dinhmartin1 <Dinh.Martin1@gmail.com>
2018-03-28 21:57:17 -0700

    Getters and setters

--- src/project/martin/VowelCounter.java
+++ src/project/martin/VowelCounter.java
@@ -3,8 +3,15 @@ package project.martin;
 public class VowelCounter {
 
 	private int numberOfA;
-	//private int numberOfA = 0;
-	
+
+	public int getnumberOfA() { 
+        return numberOfA; 
+    } 
+
+    public void setVowel(int vowel) { 
+        this.numberOfA = vowel; 
+    } 
+    
 	public void countVowels(String word) {
 		for(int i = 0; i < word.length(); i++) {
 			if(word.charAt(i) == 'A' || word.charAt(i) == 'a') {
