0d5ae0d7a3b764702bde5b980a84cb73d6549c1f
dinhmartin1 <Dinh.Martin1@gmail.com>
2018-04-18 21:28:26 -0700

    What to put to test the method in the driver

--- src/project/martin/VowelCounter.java
+++ src/project/martin/VowelCounter.java
@@ -3,14 +3,14 @@ package project.martin;
 public class VowelCounter {
 	private int numberOfA;
 
-	public VowelCounter(int vowels) {
-        this.numberOfA = vowels;
-    }
-	
-	public int getnumberOfA() { 
+	public int getNumberOfA() { 
         return numberOfA; 
     } 
 
+	public void setNumberOfA(int letterA) {
+        this.numberOfA = letterA;
+    }
+	
 	public void countVowels(String word) {
 		for(int i = 0; i < word.length(); i++) {
 			if(word.charAt(i) == 'A' || word.charAt(i) == 'a') {
--- src/project/martin/VowelCounterDriver.java
+++ src/project/martin/VowelCounterDriver.java
@@ -2,8 +2,7 @@ package project.martin;
 
 public class VowelCounterDriver {
 	public static void main(String[] args) {
-		VowelCounter occurences = new VowelCounter(5);
-		
+		VowelCounter occurences = new VowelCounter(numberOfA);
 		
 		occurences.countVowels("Abracadabra");
 		System.out.print(occurences);
