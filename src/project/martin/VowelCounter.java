e79fadae4009d22830a189a90021e0b67a51427e
dinhmartin1 <Dinh.Martin1@gmail.com>
2018-06-06 21:02:43 -0700

    Printing multiple, not specific

--- src/project/martin/VowelCounter.java
+++ src/project/martin/VowelCounter.java
@@ -2,20 +2,55 @@ package project.martin;
 
 public class VowelCounter {
 	private int numberOfA;
+	private int numberOfE;
+	private int numberOfI;
+	private int numberOfO;
+	private int numberOfU;
 
 	public int getNumberOfA() { 
         return numberOfA; 
     } 
-
-	public void setNumberOfA(int letterA) {
-        this.numberOfA = letterA;
-    }
 	
+	public int getNumberOfE() { 
+        return numberOfE; 
+    } 
+	
+	public int getNumberOfI() { 
+        return numberOfI; 
+    } 
+	
+	public int getNumberOfO() { 
+        return numberOfO; 
+    } 
+	
+	public int getNumberOfU() { 
+        return numberOfU; 
+    } 
+
 	public void countVowels(String word) {
 		for(int i = 0; i < word.length(); i++) {
 			if(word.charAt(i) == 'A' || word.charAt(i) == 'a') {
 				numberOfA++;
+				System.out.println("Number of A: ");
 			}
+			else if(word.charAt(i) == 'E' || word.charAt(i) == 'e') {
+				numberOfE++;
+				System.out.println("Number of E: ");
+			}
+			else if(word.charAt(i) == 'I' || word.charAt(i) == 'i') {
+				numberOfI++;
+				System.out.println("Number of I: ");
+			}
+			else if(word.charAt(i) == 'O' || word.charAt(i) == 'o') {
+				numberOfO++;
+				System.out.println("Number of O: ");
+			}
+			else if(word.charAt(i) == 'U' || word.charAt(i) == 'u') {
+				numberOfU++;
+				System.out.println("Number of U: ");
+			}
+			
 		}
+		//System.out.print("Number of A: ");
 	}
 }
