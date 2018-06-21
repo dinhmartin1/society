11e82fc35f01a2149b30d4431e7e1cbf9ea9cce0
dinhmartin1 <Dinh.Martin1@gmail.com>
2018-06-20 23:14:43 -0700

    Added else before if statements

--- src/project/martin/VowelCounter.java
+++ src/project/martin/VowelCounter.java
@@ -32,17 +32,17 @@ public class VowelCounter {
 			if(word.charAt(i) == 'A' || word.charAt(i) == 'a') {
 				numberOfA++;
 			}
-			if(word.charAt(i) == 'E' || word.charAt(i) == 'e') {
+			else if(word.charAt(i) == 'E' || word.charAt(i) == 'e') {
 				numberOfE++;
 			}
-			if(word.charAt(i) == 'I' || word.charAt(i) == 'i') {
+			else if(word.charAt(i) == 'I' || word.charAt(i) == 'i') {
 				numberOfI++;
 
 			}
-			if(word.charAt(i) == 'O' || word.charAt(i) == 'o') {
+			else if(word.charAt(i) == 'O' || word.charAt(i) == 'o') {
 				numberOfO++;
 			}
-			if(word.charAt(i) == 'U' || word.charAt(i) == 'u') {
+			else if(word.charAt(i) == 'U' || word.charAt(i) == 'u') {
 				numberOfU++;
 			}
 		}
